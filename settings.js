3b5d2a1c3618328d6f2c294e522e15bc953452db
ranazain3158-lab <ranazain3158@gmail.com>
2026-07-10 16:17:53 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    pexelsApiKey: process.env.PEXELS_API_KEY || 'R2hOv8iqPEUq9W9wd7vORej8qrscD0lAcxlgtUwMnc1ovSzn8QjxEi1',
+    ownerNumber: process.env.OWNER_NUMBER || '923021475390',
+    botName: '𝙍𝙖𝙣𝙖 𝙯𝙖𝙞𝙣  𝗠𝗗 𝗕𝗢𝗧',
+    ownerName: '𝙍𝙖𝙣𝙖 𝙯𝙖𝙞𝙣 '
+};
